23b42f0a66ee855f734c56597ccac2b5ae992347
Saurabh Tiwari <129385426+Rexaintreal@users.noreply.github.com>
2025-10-12 16:30:55 +0530

    Commit #29 - Added better fonts and color theme also imrpoved the overall experience of the users with minor fixes.

--- static/js/script.js
+++ static/js/script.js
@@ -81,3 +81,16 @@ if (fileUpload && !document.querySelector('.filters-page')) {
     reader.readAsDataURL(file);
   });
 }
+const backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add('visible');
+  } else {
+    backToTop.classList.remove('visible');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
